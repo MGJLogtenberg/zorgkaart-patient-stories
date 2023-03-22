bae78047289410bb31ce0e3afcb5926cdbf2aa86
MGJLogtenberg <105912903+MGJLogtenberg@users.noreply.github.com>
2023-03-22 14:00:06 +0100

    Update 6 - Combine stories and full topics.py

--- 6 - Combine stories and full topics.py
+++ 6 - Combine stories and full topics.py
@@ -6,6 +6,6 @@ import os
 df1 = pd.read_excel("final_df.xlsx",index_col=0) #Import stories
 os.chdir("C:\\Users\\mlogt\\Dropbox\\Health Humanities\\Thesis\\Results SQ1") #Change working directory
 df2 = pd.read_excel("probs.xlsx",index_col=0) #Import topics
-df = pd.concat([df1,df2],axis=1,keys='key_0',join='inner') #Join together on key (URL) to new DataFrame
+df = pd.merge(df2,df1[['key_0','review']],on='key_0') #Merge together on key (URL) to new DataFrame
 df.to_excel("probs2.xlsx") #Export
 os.chdir("C:\\Users\\mlogt\\Dropbox\\Health Humanities\\Thesis\\Code") #Change working directory to default
